86e5336ff7050dee55a6acec64cdf063f785f591
PatoGonzalezz <pato12lc@gmail.com>
2025-04-29 15:36:20 -0400

    Github Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://PatoGonzalezz.github.io',
+  base: 'mycconfecciones',
   vite: {
     plugins: [tailwindcss()]
   }
